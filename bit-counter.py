73ee2a2bc451acca664b4a3c3675a61fa63258c5
vasileybatrakov <vasileybt@gmail.com>
2026-04-27 11:48:52 +0300

    remake bit-counter

--- bit-counter.py
+++ bit-counter.py
@@ -1,29 +1,37 @@
 import RPi.GPIO as GPIO
 import time
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
-GPIO.setup(leds, GPIO.OUT)
+for led in leds:
+    GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 up = 9
 down = 10
-GPIO.setup(up, GPIO.IN)
-GPIO.setup(down, GPIO.IN)
+GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 num = 0
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
     
 sleep_time = 0.2
 
-if GPIO.input(up):
-    num = num + 1
-    print(num, dec2bin(num))
-    time.sleep(sleep_time)
+try:
+    while True:
+        if GPIO.input(up):
+            num = num + 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
 
-if GPIO.input(down):
-    num = num - 1
-    print(num, dec2bin(num))
-    time.sleep(sleep_time)
+        if GPIO.input(down):
+            num = num - 1
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+        if num < 0 or num > 255:
+            num = 0
 
-0 <= num < 256
 
-GPIO.output(leds, dec2bin(num))
+        GPIO.output(leds, dec2bin(num))
+
+except keyboardInterrupt:
+    GPIO.cleanup()
